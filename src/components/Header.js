a1be35c85605c1b19c679b620cac264cd0aec615
rittikbasu <irittik@gmail.com>
2024-02-03 23:18:53 +0530

    feat: make header text bigger for larger screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full max-w-lg mx-auto mb-4 pl-2 md:px-2 flex justify-between items-center">
-      <span className="text-3xl text-left font-bold bg-clip-text text-transparent bg-gradient-to-b from-amber-200 to-amber-500">
+    <div className="w-full max-w-xl mx-auto mb-4 pl-2 md:px-2 flex justify-between items-center">
+      <span className="md:text-4xl text-3xl text-left font-bold bg-clip-text text-transparent bg-gradient-to-b from-amber-200 to-amber-500">
         spella
       </span>
-      <span className="text-lg bg-gradient-to-r from-lime-100 via-lime-100 to-lime-50/20 rounded-l-xl font-mono text-black py-1 px-4">
+      <span className="md:text-2xl text-xl font-mono bg-gradient-to-r from-lime-100 via-lime-100 to-lime-50/20 rounded-l-xl text-black py-1 px-4">
         daily challenge
       </span>
     </div>
