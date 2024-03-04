a68ba9238c50a7573063b2c6911d25da5ed52c54
rittikbasu <irittik@gmail.com>
2024-03-04 23:57:14 +0530

    feat: remove functionality to signup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,10 @@
-import { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Header from "@/components/Header";
-import { useRouter } from "next/router";
 import { BsArrowRight } from "react-icons/bs";
 import Bee from "@/components/Bee";
-import SignupModal from "@/components/SignupModal";
-
-const PlayNowWrapper = ({ children, userExists, setShowSignup }) => {
-  const classes =
-    "bg-gradient-to-bl from-yellow-400 to-amber-500 shadow-lg text-white font-bold py-2 rounded-xl text-2xl mt-2 md:mt-8 px-8 hover:shadow-2xl active:scale-95 transition-all duration-300 outline-none";
-  return userExists ? (
-    <Link href="/daily" className={classes}>
-      {children}
-    </Link>
-  ) : (
-    <button className={classes} onClick={() => setShowSignup(true)}>
-      {children}
-    </button>
-  );
-};
 
 export default function Home() {
-  const router = useRouter();
-  const [showSignup, setShowSignup] = useState(false);
-  const [userExists, setUserExists] = useState(false);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    const id = storedUser ? JSON.parse(storedUser)?.id : null;
-
-    if (id) {
-      fetch(`/api/userExists?userId=${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          if (data.userExists) {
-            setUserExists(true);
-          } else {
-            setShowSignup(true);
-          }
-        });
-    } else {
-      if (router.query.signup === "true") {
-        setShowSignup(true);
-      }
-    }
-  }, [router.query]);
-
   return (
     <>
       <Head>
@@ -72,12 +29,15 @@ export default function Home() {
               gotta spell em&rsquo; all
             </span>
           </div>
-          <PlayNowWrapper userExists={userExists} setShowSignup={setShowSignup}>
+          <Link
+            href="/daily"
+            className="bg-gradient-to-bl from-yellow-400 to-amber-500 shadow-lg text-white font-bold py-2 rounded-xl text-2xl mt-2 md:mt-8 px-8 hover:shadow-2xl active:scale-95 transition-all duration-300 outline-none"
+          >
             <div className="flex items-center justify-center">
               play now
               <BsArrowRight className="ml-2" />
             </div>
-          </PlayNowWrapper>
+          </Link>
         </main>
         <footer className="flex items-center justify-center text-gray-600 text-sm md:text-lg py-4">
           <Link
@@ -90,7 +50,6 @@ export default function Home() {
           </Link>
         </footer>
       </div>
-      <SignupModal showSignup={showSignup} setShowSignup={setShowSignup} />
     </>
   );
 }
