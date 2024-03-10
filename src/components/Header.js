23066856135ca62a44f182c9e9e35edd0753832f
rittikbasu <irittik@gmail.com>
2024-03-11 05:29:51 +0530

    feat: add hrefs to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import clsx from "clsx";
 
 import { GiBee } from "react-icons/gi";
@@ -21,14 +22,17 @@ const Header = () => {
             svgClassName="w-7 h-7 relative md:bottom-[1.1rem] bottom-[0.9rem]"
             viewBox={"0 0 16 16"}
           />
-          <div className="md:text-5xl text-4xl font-josefin font-bold ml-[-1rem]">
+          <Link
+            href="/"
+            className="md:text-5xl text-4xl font-josefin font-bold ml-[-1rem]"
+          >
             <span className="bg-clip-text text-transparent bg-gradient-to-b from-amber-200 to-amber-500">
               spel
             </span>
             <span className="bg-clip-text text-transparent bg-gradient-to-b from-gray-500/80 to-black">
               lol
             </span>
-          </div>
+          </Link>
         </div>
         <button
           className="flex items-center gap-2 py-1 pl-4 pr-1 md:px-4 md:bg-gradient-to-t bg-gradient-to-r md:border border-y border-l border-black/5 from-lime-100/50 via-lime-100/80 to-lime-50/50 md:rounded-xl rounded-l-xl outline-none"
@@ -68,10 +72,8 @@ const GradientIcon = ({
 
 const MenuModal = ({ setModalOpen }) => {
   const [isOpen, setIsOpen] = useState(false);
-
-  useEffect(() => {
-    setIsOpen(true);
-  }, []);
+  const router = useRouter();
+  const isCurrentPage = (href) => router.pathname === href;
 
   const handleClose = () => {
     setIsOpen(false);
@@ -79,10 +81,14 @@ const MenuModal = ({ setModalOpen }) => {
       setModalOpen(false);
     }, 200);
   };
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
   return (
     <div
       className={clsx(
-        "fixed inset-0 z-10 bg-black bg-opacity-30 flex justify-center items-center transition-opacity duration-700",
+        "fixed inset-0 z-10 bg-black bg-opacity-30 flex justify-center items-center transition-opacity duration-300",
         isOpen
           ? "opacity-100 backdrop-blur-2xl"
           : "opacity-0 backdrop-blur-sm pointer-events-none"
@@ -90,37 +96,55 @@ const MenuModal = ({ setModalOpen }) => {
     >
       <div
         className={clsx(
-          "relative bg-white bg-opacity-30 py-12 md:py-14 rounded-3xl w-4/5 max-w-md flex justify-center transition-all duration-500 transform",
+          "relative bg-white bg-opacity-30 py-12 md:py-14 rounded-3xl w-4/5 max-w-sm flex justify-center transition-all duration-500 transform",
           isOpen ? "scale-100" : "scale-50"
         )}
       >
         <ul className="space-y-8 text-2xl md:text-3xl text-gray-800 tracking-wide">
           <Link
             href="/daily"
-            className="flex items-center gap-6 cursor-pointer hover:text-blue-600"
+            className={clsx(
+              "flex items-center gap-6 cursor-pointer",
+              isCurrentPage("/daily") ? "text-lime-200" : "hover:text-lime-200"
+            )}
           >
-            <BsSpellcheck className="" />
+            <BsSpellcheck />
             daily
           </Link>
           <Link
-            href=""
-            className="flex items-center gap-6 cursor-pointer hover:text-blue-600"
+            href="/practice"
+            className={clsx(
+              "flex items-center gap-6 cursor-pointer",
+              isCurrentPage("/practice")
+                ? "text-lime-200"
+                : "hover:text-lime-200"
+            )}
           >
-            <TbTargetArrow className="" />
+            <TbTargetArrow />
             practice
           </Link>
           <Link
-            href=""
-            className="flex items-center gap-6 cursor-pointer hover:text-blue-600"
+            href="/leaderboard"
+            className={clsx(
+              "flex items-center gap-6 cursor-pointer",
+              isCurrentPage("/leaderboard")
+                ? "text-lime-200"
+                : "hover:text-lime-200"
+            )}
           >
-            <PiFlagCheckeredDuotone className="" />
+            <PiFlagCheckeredDuotone />
             leaderboard
           </Link>
           <Link
-            href=""
-            className="flex items-center gap-6 cursor-pointer hover:text-blue-600"
+            href="/credits"
+            className={clsx(
+              "flex items-center gap-6 cursor-pointer",
+              isCurrentPage("/credits")
+                ? "text-lime-200"
+                : "hover:text-lime-200"
+            )}
           >
-            <LuHeartHandshake className="" />
+            <LuHeartHandshake />
             credits
           </Link>
         </ul>
